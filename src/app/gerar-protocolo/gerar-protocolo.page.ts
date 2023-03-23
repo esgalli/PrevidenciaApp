c9ed19bdea0c9f38908e2d4a79c90312d8b2c1df
renanrmolina <renanrobertomolina@hotmail.com>
2023-03-22 22:18:18 -0300

    gerar-protocolo

---
+++ src/app/gerar-protocolo/gerar-protocolo.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gerar-protocolo',
+  templateUrl: './gerar-protocolo.page.html',
+  styleUrls: ['./gerar-protocolo.page.scss'],
+})
+export class GerarProtocoloPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
