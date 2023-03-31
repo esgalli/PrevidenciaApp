4361d1b7b3f3ba6440668b5b0e84c00914fbcab6
RicardoLacerda96 <ricardogarcialacerda125@gmail.com>
2023-03-30 21:21:38 -0300

    revisar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '/login',
     pathMatch: 'full'
   },
+  {
+    path: 'revisar',
+    loadChildren: () => import('./revisar/revisar.module').then( m => m.RevisarPageModule)
+  }
 ];
 
 @NgModule({
@@ -19,4 +27,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/revisar/revisar.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-revisar',
+  templateUrl: './revisar.page.html',
+  styleUrls: ['./revisar.page.scss'],
+})
+export class RevisarPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
