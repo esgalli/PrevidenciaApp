d294121d8fc3be4ff2168cf7296c051e6edce76f
Everton Steiner Galli <everton.galli@etecbarrabonita.com.br>
2023-04-13 21:32:38 -0300

    Atualizar Cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,24 +16,8 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'gerar-protocolo',
-    loadChildren: () => import('./gerar-protocolo/gerar-protocolo.module').then( m => m.GerarProtocoloPageModule)
-  },
-  {
-    path: 'revisar',
-    loadChildren: () => import('./revisar/revisar.module').then( m => m.RevisarPageModule)
-  },
-  {
-    path: 'laudos',
-    loadChildren: () => import('./laudos/laudos.module').then( m => m.LaudosPageModule)
-  },
-  {
-    path: 'agendar-pericia',
-    loadChildren: () => import('./agendar-pericia/agendar-pericia.module').then( m => m.AgendarPericiaPageModule)
-  },
-  {
-    path: 'prova-vida',
-    loadChildren: () => import('./prova-vida/prova-vida.module').then( m => m.ProvaVidaPageModule)
+    path: 'atualizar',
+    loadChildren: () => import('./atualizar/atualizar.module').then( m => m.AtualizarPageModule)
   }
 ];
 
