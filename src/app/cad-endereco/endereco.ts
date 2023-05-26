610a7d212bf886fbbbcbfe3ad359b0100e62658f
esgalli <59775186+esgalli@users.noreply.github.com>
2023-05-26 20:17:05 -0300

    Feature/camera (#10)
    * Configuração de plugin Camera
    
    * inserindo api
    
    * Implementacao API ViaCep
    
    ---------
    
    Co-authored-by: Everton Steiner Galli <everton.galli@etecbarrabonita.com.br>

---
+++ src/app/cad-endereco/endereco.ts
@@ -0,0 +1,13 @@
+export class Endereco {
+  cep: string = '';
+  logradouro: string = '';
+  complemento: string = '';
+  bairro: string = '';
+  localidade: string = '';
+  uf: string = '';
+  ibge: string = '';
+  gia: string = '';
+  ddd: string = '';
+  siafi: string = '';
+  numero: string = '';
+}
